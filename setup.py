e9bba940d31c246765b4c25f1e074f6939d6b3e6
Athon Millane <athon@maxkelsen.com>
2020-10-06 15:06:11 +1000

    Fix dodgy setup.py file.

--- setup.py
+++ setup.py
@@ -3,13 +3,14 @@ from setuptools import setup, find_packages
 with open("README.md") as f:
     readme = f.read()
 
+with open('requirements.txt') as f:
+    deps = f.read().splitlines()
+
 setup(
     name="pgportfolio",
     version="1.0.0",
-    description="",
+    install_requires=deps,
     long_description=readme,
     author="Zhengyao Jiang, Dixing Xu, Jinjun Liang",
-    author_email="",
-    packages=find_packages(exclude=("tests", "docs"),
-                           include=("matplotlib", "tensorflow", "tflearn", "pandas",
-                                    "pandas", "cvxopt", "scipy")))
+    packages=find_packages(),
+)
